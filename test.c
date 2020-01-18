7937dfa15e6fcf60bf0068dffd454a887455ea62
Ezaki-Yuko <2473955214@qq.com>
2020-01-18 17:04:10 +0800

    新增文件

---
+++ test.c
@@ -0,0 +1,23 @@
+//shift + alt + enter 编辑区最大化
+
+//包含头文件.想象成一个超市
+//C语言中有很多头文件，不同的头文件中包含了不同的库函数（面条）
+//stdio => printf
+//#include <stdio.h>
+//stdlib.h => system
+//#include<stdlib.h>
+
+//主函数，一个程序的入口,一个C语言程序必须有且仅有一个main函数
+//int main() {
+	//注释 不会参与运行，提醒程序员更好地理解这段程序
+	//print 打印（显示在屏幕上）
+	//f format 格式
+	//printf("hehe");
+	//system("pause");
+	//函数返回了 执行到这个return0 程序就执行结束了
+	//return 0;
+//}
+//Ctrl+F5做了两件事 编译+运行
+//1.把.c源代码文件转换成了.exe的可执行程序（可编译）
+//2.运行exe（双击）
+//变量 
